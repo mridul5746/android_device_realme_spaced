00290b6bc22c280f8af87d26fa4ead91ee7e6809
bengris32 <bengris32@protonmail.ch>
2024-06-28 18:44:46 +0100

    spaced: Ship latest cgroups and task_profiles configurations
    * libprocessgroup will by default try to use the older versions
      of these configs since we specify launch API level 30.
    
    Change-Id: Iaa535df897c7337d5f04e90c4252cab31a614123
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -99,10 +99,10 @@ PRODUCT_PACKAGES += \
     libcamera_metadata.vendor \
     libcamera_metadata_shim
 
-# Cgroup
+# Cgroups
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Display
 PRODUCT_PACKAGES += \
