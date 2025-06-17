aac274419ba5a71ba414c045afdfdd7833e0e514
HELLINFIX <mridulsharma57570@gmail.com>
2025-06-17 08:09:49 +0000

    spaced: pixify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest_odm.xml
 
 ODM_MANIFEST_SKUS += nfc
